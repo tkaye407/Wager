1fb621aa63e7508ad5c34f6eed6bbc7fd6938fda
tkaye407 <tkaye@princeton.edu>
2017-04-03 12:06:10 -0400

    added wager on launch screen

---
+++ WagerApp/BetListTableViewController.swift
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2015 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import Firebase
+
+class BetListTableViewController: UITableViewController {
+
+  // MARK: Constants
+  let listToUsers = "ListToUsers"
+  
+  // MARK: Properties 
+  let ref = FIRDatabase.database().reference(withPath: "Bets")
+  var items: [BetItem] = []
+  var user: User!
+  var userCountBarButtonItem: UIBarButtonItem!
+  
+  // MARK: UIViewController Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    tableView.allowsMultipleSelectionDuringEditing = false
+    
+    userCountBarButtonItem = UIBarButtonItem(title: "1",
+                                             style: .plain,
+                                             target: self,
+                                             action: #selector(userCountButtonDidTouch))
+    userCountBarButtonItem.tintColor = UIColor.white
+    navigationItem.leftBarButtonItem = userCountBarButtonItem
+    
+    user = User(uid: "FakeId", email: "hungry@person.food")
+    
+    ref.observe(.value, with: { snapshot in
+      var newItems: [BetItem] = []
+      for item in snapshot.children {
+        let betItem = BetItem(snapshot: item as! FIRDataSnapshot)
+        newItems.append(betItem)
+      }
+      self.items = newItems
+      self.tableView.reloadData()
+    })
+  }
+  
+  // MARK: UITableView Delegate methods
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+    let betItem = items[indexPath.row]
+    
+    cell.textLabel?.text = betItem.name
+    cell.detailTextLabel?.text = betItem.challenger
+    
+    toggleCellCheckbox(cell, isCompleted: betItem.completed)
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      items.remove(at: indexPath.row)
+      tableView.reloadData()
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) else { return }
+    var betItem = items[indexPath.row]
+    let toggledCompletion = !betItem.completed
+    
+    toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+    betItem.completed = toggledCompletion
+    tableView.reloadData()
+  }
+  
+  func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
+    if !isCompleted {
+      cell.accessoryType = .none
+      cell.textLabel?.textColor = UIColor.black
+      cell.detailTextLabel?.textColor = UIColor.black
+    } else {
+      cell.accessoryType = .checkmark
+      cell.textLabel?.textColor = UIColor.gray
+      cell.detailTextLabel?.textColor = UIColor.gray
+    }
+  }
+  
+  // MARK: Add Item
+  
+  @IBAction func addButtonDidTouch(_ sender: AnyObject) {
+    let alert = UIAlertController(title: "New Bet", message: "Enter Description", preferredStyle: .alert)
+    
+    let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+        guard let textField = alert.textFields?.first,
+          let text = textField.text else { return }
+        
+        let betItem = BetItem(name: text, challenger: self.user.email, completed: false)
+        let betItemRef = self.ref.child(text.lowercased())
+        betItemRef.setValue(betItem.toAnyObject())
+    }
+    let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+    alert.addTextField()
+    alert.addAction(saveAction)
+    alert.addAction(cancelAction)
+    present(alert, animated: true, completion: nil)
+  }
+  
+  func userCountButtonDidTouch() {
+    performSegue(withIdentifier: listToUsers, sender: nil)
+  }
+  
+}
