a39a2a10639a58fa472ce9fa2a2e047e9f057a18
Will Chance <wchance@princeton.edu>
2017-04-10 20:55:34 -0400

    change to profile and rating

--- Grocr/ProfileEditorViewController.swift
+++ Grocr/ProfileEditorViewController.swift
@@ -41,7 +41,12 @@ class ProfileEditorViewController: UIViewController {
         let userID = "ex"
         let pnl = Float(0.0)*/
       
-      profile = Profile(firstName: firstNameTextField.text ?? (profile?.firstName)!, lastName: lastNameTextField.text ?? (profile?.lastName)!)
+      profile = Profile(firstName: firstNameTextField.text ?? (profile?.firstName)!, lastName: lastNameTextField.text ?? (profile?.lastName)!, email: emailTextField.text ?? "", venmoID: venmoIDTextField.text ?? "", gender: genderTextField.text ?? "")
     }
     
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+  
 }
