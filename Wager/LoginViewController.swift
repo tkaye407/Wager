6e307477aff72bb719e31ab11d4dee8f13eed685
Michael Swart <mswart@princeton.edu>
2017-04-27 20:35:37 -0400

    now have option to deny a bet as well as complete

--- Wager/LoginViewController.swift
+++ Wager/LoginViewController.swift
@@ -24,41 +24,6 @@ class LoginViewController: UIViewController {
                            password: textFieldLoginPassword.text!)
   }
   
-  /*@IBAction func signUpDidTouch(_ sender: AnyObject) {
-    let alert = UIAlertController(title: "Register",
-                                  message: "Register",
-                                  preferredStyle: .alert)
-    
-    let saveAction = UIAlertAction(title: "Save", style: .default) { action in
-      let emailField = alert.textFields![0]
-      let passwordField = alert.textFields![1]
-      
-      FIRAuth.auth()!.createUser(withEmail: emailField.text!,
-                                 password: passwordField.text!) { user, error in
-          if error == nil {
-            FIRAuth.auth()!.signIn(withEmail: self.textFieldLoginEmail.text!, password: self.textFieldLoginPassword.text!)
-          }
-      }
-    }
-    
-    let cancelAction = UIAlertAction(title: "Cancel",
-                                     style: .default)
-    
-    alert.addTextField { textEmail in
-      textEmail.placeholder = "Enter your email"
-    }
-    
-    alert.addTextField { textPassword in
-      textPassword.isSecureTextEntry = true
-      textPassword.placeholder = "Enter your password"
-    }
-    
-    alert.addAction(saveAction)
-    alert.addAction(cancelAction)
-    
-    present(alert, animated: true, completion: nil)
-  }*/
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.tabBarController?.tabBar.isHidden = true
