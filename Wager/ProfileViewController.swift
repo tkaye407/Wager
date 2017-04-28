4a934725f7fffbbae8868f76fae863164978e69f
Will Chance <wchance@princeton.edu>
2017-04-27 21:49:56 -0400

    remove button from profile table

--- Wager/ProfileViewController.swift
+++ Wager/ProfileViewController.swift
@@ -265,9 +265,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
       let cell = self.betsTableView.dequeueReusableCell(withIdentifier: "ProfileBetTableViewCell") as! ProfileBetTableViewCell
       
       cell.betNameLabel.text = betItem.name
-      cell.actionButton.setTitle("Button", for: [])
       
-      cell.actionButton.addTarget(self, action: #selector(ProfileViewController.sayHi), for: UIControlEvents.touchUpInside)
       return cell
       
     }
