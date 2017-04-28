734ec1813907797608f3a1022c6cd363dcd431ca
Richard Bush <rjb2@princeton.edu>
2017-04-27 21:29:28 -0400

    login page overhaul

--- Wager/LoginViewController.swift
+++ Wager/LoginViewController.swift
@@ -14,15 +14,18 @@ class LoginViewController: UIViewController {
   let loginToList = "LoginToList"
   let test = 1
   
+  @IBOutlet weak var scrollView: UIScrollView!
   // MARK: Outlets
   @IBOutlet weak var textFieldLoginEmail: UITextField!
   @IBOutlet weak var textFieldLoginPassword: UITextField!
+  @IBOutlet weak var signupButton: UIButton!
   
   // MARK: Actions
   @IBAction func loginDidTouch(_ sender: AnyObject) {
     FIRAuth.auth()!.signIn(withEmail: textFieldLoginEmail.text!,
                            password: textFieldLoginPassword.text!)
   }
+
   
   /*@IBAction func signUpDidTouch(_ sender: AnyObject) {
     let alert = UIAlertController(title: "Register",
@@ -61,6 +64,21 @@ class LoginViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    self.scrollView.isScrollEnabled = false
+    
+    // some bullshit to allow me to mess with the keybaord
+    NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    
+    
+    signupButton.layer.borderColor = UIColor.white.cgColor
+   
+    textFieldLoginEmail.borderStyle = .roundedRect
+    
+    textFieldLoginPassword.borderStyle = .roundedRect;
+    
     self.tabBarController?.tabBar.isHidden = true
     
     FIRAuth.auth()!.addStateDidChangeListener() { auth, user in
@@ -70,9 +88,28 @@ class LoginViewController: UIViewController {
     }
     self.navigationController?.navigationBar.topItem?.leftBarButtonItem = nil;
   }
+  
+  func keyboardWillShow(notification:NSNotification) {
+    let userInfo:NSDictionary = notification.userInfo! as NSDictionary
+    let keyboardFrame:NSValue = userInfo.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+    let keyboardRectangle = keyboardFrame.cgRectValue
+    let keyboardHeight = keyboardRectangle.height
+    let height = UIScreen.main.bounds.size.height-keyboardHeight+25
+    self.scrollView.isScrollEnabled = true
+self.scrollView.contentSize=CGSize(width: self.scrollView.contentSize.width, height: height)
+    
+   
+    
+  }
+  
+  func keyboardWillHide(notification:NSNotification) {
+    self.scrollView.setContentOffset(CGPoint(x: 0, y:0), animated: true)
+    self.scrollView.isScrollEnabled = false
+  }
 }
 
-extension LoginViewController: UITextFieldDelegate {
+
+  extension LoginViewController: UITextFieldDelegate {
   
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     if textField == textFieldLoginEmail {
@@ -80,8 +117,11 @@ extension LoginViewController: UITextFieldDelegate {
     }
     if textField == textFieldLoginPassword {
       textField.resignFirstResponder()
+self.scrollView.setContentOffset(CGPoint(x: 0, y:0), animated: true)
     }
+    
     return true
   }
   
-}
+  }
+
